525a87d7c6590a2a0a9abdd9add703c1191c787f
Peitao <peitao@meituan.com>
2017-12-04 17:31:49 +0800

    add shell project

---
+++ ios/Griffin/Griffin/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Griffin
+//
+//  Created by 裴韬 on 2017/12/4.
+//  Copyright © 2017年 裴韬. All rights reserved.
+//
+
+import UIKit
+import GriffinSDK
+import JavaScriptCore
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
