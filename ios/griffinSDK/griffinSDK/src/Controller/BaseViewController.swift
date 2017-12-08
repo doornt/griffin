6c68470d40ceb28083c99445863d0398692e78aa
Sampson <dujianwxfdt@live.cn>
2017-12-08 15:33:57 +0800

    baseviewcontroller

--- ios/griffinSDK/griffinSDK/src/Controller/BaseViewController.swift
+++ ios/griffinSDK/griffinSDK/src/Controller/BaseViewController.swift
@@ -11,20 +11,39 @@ import UIKit
 
 public class BaseViewController : UIViewController,UIGestureRecognizerDelegate{
     
+    var sourceUrl:NSURL?
+    
+    convenience init(url:NSURL){
+        self.init(nibName:nil,bundle:nil)
+        
+        self.sourceUrl = url
+    }
+    
+    private func renderWithUrl(){
+        if sourceUrl == nil{
+            return
+        }
+        
+        
+    }
+    
     
     override public func viewDidAppear(_ animated: Bool) {
-        
+        super.viewDidAppear(animated)
     }
     
     override public func viewDidLoad() {
-        
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        self.renderWithUrl()
     }
     
     override public func viewDidDisappear(_ animated: Bool) {
-        
+        super.viewDidAppear(animated)
     }
     
     override public func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
         
     }
 }
