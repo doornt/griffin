05653560abc830f617c9244c1395e7dff174d163
Peitao <peitao@meituan.com>
2017-12-25 14:31:09 +0800

    update

--- ios/griffinSDK/griffinSDK/src/Engine/Engine.swift
+++ ios/griffinSDK/griffinSDK/src/Engine/Engine.swift
@@ -51,8 +51,8 @@ public class Engine: NSObject {
        
         
         let registerEvent:@convention(block)(UIView, String, String)-> Void = {
-            view, event, callBackString in
-            RenderManager.instance.registerEvent(view, event: event, callBackString: callBackString)
+            view, event, callBack in
+            RenderManager.instance.registerEvent(view, event: event, callBack: callBack)
         }
         
         JSCoreBridge.instance.getContext().setObject(unsafeBitCast(createViewScript,to: AnyObject.self) , forKeyedSubscript: "createView" as NSCopying & NSObjectProtocol)
@@ -61,9 +61,8 @@ public class Engine: NSObject {
         JSCoreBridge.instance.getContext().setObject(unsafeBitCast(setRootViewScript,to: AnyObject.self) , forKeyedSubscript: "setRootView" as NSCopying & NSObjectProtocol)
         JSCoreBridge.instance.getContext().setObject(unsafeBitCast(useElementScript,to: AnyObject.self) , forKeyedSubscript: "useElement" as NSCopying & NSObjectProtocol)
         JSCoreBridge.instance.getContext().setObject(unsafeBitCast(addSubviewScript,to: AnyObject.self) , forKeyedSubscript: "addSubview" as NSCopying & NSObjectProtocol)
-        JSCoreBridge.instance.getContext().setObject(unsafeBitCast(consoleLog, to: AnyObject.self),forKeyedSubscript: "consoleLog" as NSCopying & NSObjectProtocol)
         JSCoreBridge.instance.getContext().setObject(unsafeBitCast(registerEvent, to: AnyObject.self),forKeyedSubscript: "registerEvent" as NSCopying & NSObjectProtocol)
-        
+
         JSCoreBridge.instance.getContext().exceptionHandler = {(ctx: JSContext!, value: JSValue!) in
             let stacktrace = value.objectForKeyedSubscript("stack").toString()
             let lineNumber = value.objectForKeyedSubscript("line")
@@ -72,7 +71,6 @@ public class Engine: NSObject {
             
             print("\nJS ERROR: \(value) \(moreInfo)")
         }
-        
     }
     
    
