62605a7e30479b77ae71d31a4732faf48d1120de
Sampson <dujianwxfdt@live.cn>
2018-01-06 22:27:48 +0800

    bug

--- ios/griffinSDK/griffinSDK/src/Layout/Component+Layout.swift
+++ ios/griffinSDK/griffinSDK/src/Layout/Component+Layout.swift
@@ -23,6 +23,10 @@ extension ViewComponent{
     func layoutFinish(){
         let view:UIView = self.loadView()
         
+        if layout._layout_frame.width == 0{
+            return
+        }
+        
         view.frame = CGRect(x: layout._layout_frame.x, y: layout._layout_frame.y, width: layout._layout_frame.width, height: layout._layout_frame.height)
         
     }
